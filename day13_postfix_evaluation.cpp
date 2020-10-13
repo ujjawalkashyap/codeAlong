c68e92e23dbb1356830917dff14b72857054c2bb
ujjawalkashyap <32612153+ujjawalkashyap@users.noreply.github.com>
2020-10-13 23:12:54 +0530

    fixed yesterday's code

--- day13_postfix_evaluation.cpp
+++ day13_postfix_evaluation.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int evaluate(string s){
     stack<int> ch;
     for(int index=0;index<s.length();index++){
-        if(s[index]!='*'or s[index]!='+'or s[index]!='-'){
-            ch.push(s[index]);
-        }
+        if(s[index]!='*'and s[index]!='+'and s[index]!='-'){
+            ch.push(s[index]-'0');
+            //cout<<s[index]-'0'<<'\n';
+
+}
         else if(s[index]=='*'or s[index]=='+'or s[index]=='-'){
-            int op1=ch.top();
+            int op1=(int)ch.top();
             ch.pop();
-            int op2=ch.top();
+            int op2=(int)ch.top();
             ch.pop();
             int result;
             if(s[index]=='*')
@@ -19,8 +21,9 @@ int evaluate(string s){
             if(s[index]=='+')
                 result=op1+op2;
             if(s[index]=='-')
-                result=op1-op2;
+                result=op2-op1;
             ch.push(result);
+            //cout<<result<<'\n';
         }
 
     }
